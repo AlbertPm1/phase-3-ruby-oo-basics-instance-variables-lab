4df4e3a9cf80d192d7f65f42cc03cfeceb61b64e
Albertpm1 <petermburunganga98@gmail.com>
2023-02-13 08:10:55 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
+# parent class
 class Dog
 
-end
+    # bark instance
+    def bark
+        puts "Woof!"    
+        end
+    
+    
+    # sit instance
+    def sit
+         puts "The Dog is sitting" 
+        end
+    end
